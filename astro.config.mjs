13a721f99bceb730907b57e94e50a368252b9ccc
magizebi <magizebi25@gmail.com>
2024-11-06 16:12:27 +0900

    change deploy file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://magizebi.github.io',
+  base: '/',
   devToolbar: {
     enabled: false,
   },
